c7d9c81db6fd07b905a17d3a8875fdd71fd5e957
DILIP SARVAIYA <dilipsarvaiya992@gmail.com>
2023-08-18 18:34:25 +0530

    react setup done

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const AppLayout = () => <div className="app">this is a react</div>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
